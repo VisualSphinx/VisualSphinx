db57341d7eda32114d60fa4f93f5af6e4c4db963
yichen-f <yfeng42@uw.edu>
2025-05-17 21:28:20 -0700

    update api template

---
+++ pipeline/api_config.py
@@ -0,0 +1,19 @@
+TOGETHER_API_KEY = ""
+
+OpenAI_API_KEY_1 = ""
+
+OpenAI_API_KEY = ""
+
+XAI_API_KEY = ""
+
+XAI_API_KEYS = [
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+
+Anthropic_API_KEY = ""
+
+Gemini_API_KEY = ""
